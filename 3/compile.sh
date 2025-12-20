39d48373ad49d3197ef913a3458d329a625c8d9f
dozorcevae <lena.dozorceva@gmail.com>
2025-12-20 16:52:50 +0700

    mini fix

--- 3/compile.sh
+++ 3/compile.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-gcc -Wall -Wextra -O2 -std=c11 -Include -o server server.c
+gcc -Wall -Wextra -O2 -std=c11 -Iinclude -o server server.c
 gcc -Wall -Wextra -O2 -std=c11 -Iinclude -o client \
 	src/client_main.c src/client_args.c src/ipc_client.c src/record.c src/scanner.c src/util.c
 
